c69aa95e9002123158b73f74f77b347b90200cad
Yen Szen <55312065+yenszen@users.noreply.github.com>
2020-02-07 12:30:07 +0800

    added styling tweaks, started working on calendar functionality for stretch

---
+++ src/components/Calendar.js
@@ -0,0 +1,20 @@
+/*
+    logic breakdown
+    mutate the GET url - specifically the date part of the url
+    create 3 buttons
+        - one to go back a previous day
+        - on to go forward to the next day
+        - reset 
+    use if statement to restrict the number of days between months
+*/
+import React from "react";
+
+const Calendar = () => {
+    return (
+        <div>
+        
+        </div>
+    );
+}
+
+export default Calendar;
--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -15,10 +15,34 @@ const Text = styled.div`
     justify-content: center;
 `;
 
+const Display = styled.div`
+    width: 35%;
+    margin: 0 auto;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+`;
+
+const Button = styled.button`
+    background: rgb(0, 0, 0);
+    color: gold;
+    border: black;
+    width: 6rem;
+    height: 2rem;
+    border-radius: 12px;
+`;
+
 const PhotoCard = props => {
     return (
         <ApodCard>
             <p>{props.date}</p>
+
+            <Display>
+                <Button>Previous Day</Button>
+                <Button>Next Day</Button>
+                <Button>Reset</Button>
+            </Display>
+            
             <h3>{props.title}</h3>
             <img className="image" alt={props.title} src={props.image} />
             <Text>
